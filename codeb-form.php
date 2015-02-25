9c4ba1101820d009654fcadfc8c00303dea5c5ba
Surabhi Chauhan <surabhichauhan.27@gmail.com>
2015-02-25 12:09:32 +0530

    basic files

---
+++ codeb-form.php
@@ -0,0 +1,33 @@
+<?php
+/*
+Plugin Name: Codeb Form
+Description: Form plugin for codebibber website
+Aurthor: Surabhi Chauhan
+Author URI: https://www.youtube.com/user=bellacynosure
+Version: 1
+*/
+
+global $wpdb;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
