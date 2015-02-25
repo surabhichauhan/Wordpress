829d8aae98b82539c879940d7fc7e074afb355c9
Surabhi Chauhan <surabhichauhan.27@gmail.com>
2015-02-25 13:11:26 +0530

    display form

--- codeb-form.php
+++ codeb-form.php
@@ -9,25 +9,17 @@ Version: 1
 
 global $wpdb;
 
+function CodebFormInclude(){
+	include( 'includes/form.php' );
+}
 
+// Register a new shortcode: 
+add_shortcode('custom_form', 'custom_form_shortcode');
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// The callback function that will replace [book]
+function custom_form_shortcode() {
+    ob_start();
+    CodebFormInclude();
+    return ob_get_clean();
+}
 ?>
