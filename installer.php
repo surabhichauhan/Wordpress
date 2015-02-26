5d74439ced9d8ee9e090a0e6c22663509fd2c601
Surabhi Chauhan <surabhichauhan.27@gmail.com>
2015-02-26 17:33:54 +0530

    ....

--- installer.php
+++ installer.php
@@ -2,16 +2,10 @@
 //***** Installer *****
 global $wp_version, $wpdb;
 
-if ( version_compare( $wp_version, '3.0', '<' ) ) {
-	require_once( ABSPATH . 'wp-admin/upgrade.php' );
-} else {
-	require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
-}
+function create_db() {
 
-if( $wpdb->get_var( 'SHOW TABLES LIKE "' . SUBTABLE . '"' ) != SUBTABLE ) 
-{
 	$sql = 'CREATE TABLE'  .SUBTABLE. ' (
-		  id int(12) NOT NULL auto_increment,
+		 
 		  user_name varchar(150) NOT NULL,
 		  gender varchar(250) NOT NULL,
 		  technical_stack varchar(500) NOT NULL,
@@ -24,13 +18,12 @@ if( $wpdb->get_var( 'SHOW TABLES LIKE "' . SUBTABLE . '"' ) != SUBTABLE )
 		  cover text NOT NULL,
 		  resume text NOT NULL,
 		  pubdate datetime NOT NULL,
-		  PRIMARY KEY  (id)
+		 
 		);';
+  dbDelta( $sql );
+  require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+
+
 }
-require( ABSPATH . 'wp-admin/includes/upgrade.php' );
-
-	dbDelta( $sql );
-
-add_option( 'resume_widget_title', 'CV Submission' );
-
+register_activation_hook(__FILE__,'create_db');
 ?>
