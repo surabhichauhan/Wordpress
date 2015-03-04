ef60b43ef681c296876128e19e59f90b3cea9e97
Surabhi Chauhan <surabhichauhan.27@gmail.com>
2015-03-04 13:51:19 +0530

    pippins tutorial's end

--- just-plugin/includes/admin-page.php
+++ just-plugin/includes/admin-page.php
@@ -4,6 +4,7 @@ function jp_options_page(){
 
     global $jp_options;
 
+
 	ob_start(); ?>
 	<div class="wrap">
 	<h2>just plugin's option page</h2>
@@ -11,10 +12,30 @@ function jp_options_page(){
 
 <form method="post" action="options.php">
 <?php settings_fields('jp_settings_group'); ?>
+<!--
+<h4><?php _e('Enable:','jp_domain'); ?></h4>
+<p><input id="jp_settings[enable]" name="jp_settings['enable']" type="checkbox" value="1" <?php checked(1,$jp_options['enable']); ?> />
+<label class="description"  for="jp_settings[enable]"><?php _e('display follow me link?','jp_domain'); ?> </label>
+</p>
+-->
 <h4>good reads information </h4>
 <p>
-  <label class="description" for="jp_settings[goodreads_url]"><?php _e('enter your goodreads URL','jp_domain'); ?></label>
+  
 <input id="jp_settings[goodreads_url]" name="jp_settings[goodreads_url]" type="text" value="<?php echo $jp_options["goodreads_url"] ?>" />
+<label class="description" for="jp_settings[goodreads_url]"><?php _e('enter your goodreads URL:','jp_domain'); ?></label>
+</p>
+
+<h4><?php _e('Themes:','jp_domain'); ?> </h4>
+<p>
+<?php $styles = array('blue','red'); ?> 
+<select name="jp_settings[themes]" id="jp_settings[themes]">
+<?php  foreach ($styles as $style ) { ?>
+<?php if($jp_options['themes'] == $style) { $selected = 'selected="selected"'; } else { $selected = "";} ?>
+<option value="<?php echo $style; ?>" <?php echo $selected; ?>><?php echo $style ?></option>	
+ <?php } ?>
+
+</select>
+
 </p>
 
 <p class="submit">
--- just-plugin/includes/display-functions.php
+++ just-plugin/includes/display-functions.php
@@ -6,8 +6,8 @@ function jp_display_content($content){
 
 	 global $jp_options;
 
-	if(is_single())
-	{   $extra_content = '<p class="goodreads-message">follow me on <a href=" ' . $jp_options[goodreads_url] . '">Good Reads</a></p>';
+	if(is_single() )
+	{   $extra_content = '<p class="goodreads-message ' . $jp_options['themes'] . ' ">follow me on <a href=" ' . $jp_options['goodreads_url'] . '">Good Reads</a></p>';
 		$content .= $extra_content;
 	}
 	return $content ;
