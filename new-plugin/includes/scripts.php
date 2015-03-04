a47decb0f0bc174925acf4b971d331ed96a2ec0d
Surabhi Chauhan <surabhichauhan.27@gmail.com>
2015-03-04 17:06:46 +0530

    ..

--- new-plugin/includes/scripts.php
+++ new-plugin/includes/scripts.php
@@ -3,7 +3,7 @@
 /****Script Control****/
 
 function np_load_scripts(){
-	wp_enqueue_style('jp-styles',plugin_dir_url(__FILE__) . 'css/style.css');
+	wp_enqueue_style('np-styles',plugin_dir_url(__FILE__) . 'css/style.css');
 }
 
-add_action('wp_enqueue_scripts','jp_load_scripts');
+add_action('wp_enqueue_scripts','np_load_scripts');
